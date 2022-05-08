a3481359544ecc2f3f2d0828f04bfd0eb501894e
konstantinovaw <kamikofuroraru@gmail.com>
2022-05-08 16:12:23 +0300

    lucky-tickets: junior ticket

--- src/main/java/com/konstantinova/algorithms/Main.java
+++ src/main/java/com/konstantinova/algorithms/Main.java
@@ -1,7 +1,11 @@
 package com.konstantinova.algorithms;
 
+import com.konstantinova.algorithms.luckytickets.LuckyTickets;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hi, my name is Veroshka");
+        //System.out.println("hi, my name is Veroshka");
+        LuckyTickets luckyTickets = new LuckyTickets();
+        System.out.println(luckyTickets.juniorTicket());
     }
 }
