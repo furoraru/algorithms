906256c593c480b2f038a46d4867a6a07cf8c5fa
furoraru <kamikofuroraru@gmail.com>
2022-05-24 16:55:23 +0300

    bitwise-arithmetic: refactor test

--- src/test/java/com/konstantinova/algorithms/bitwisearithmetic/BitwiseArithmeticTest.java
+++ src/test/java/com/konstantinova/algorithms/bitwisearithmetic/BitwiseArithmeticTest.java
@@ -8,39 +8,39 @@ import java.io.IOException;
 import java.math.BigInteger;
 
 public class BitwiseArithmeticTest {
-    private static final String pathKing = "src/main/resources/bitwisearithmetic/king/test.";
-    private static final String pathKnight = "src/main/resources/bitwisearithmetic/knight/test.";
-    private static final String pathRook = "src/main/resources/bitwisearithmetic/rook/test.";
-    private static final String pathBishop = "src/main/resources/bitwisearithmetic/bishop/test.";
-    private static final String PathQueen = "src/main/resources/bitwisearithmetic/queen/test.";
+    private static final String PATH_KING = "src/main/resources/bitwisearithmetic/king/test.";
+    private static final String PATH_KNIGHT = "src/main/resources/bitwisearithmetic/knight/test.";
+    private static final String PATH_ROOK = "src/main/resources/bitwisearithmetic/rook/test.";
+    private static final String PATH_BISHOP = "src/main/resources/bitwisearithmetic/bishop/test.";
+    private static final String PATH_QUEEN = "src/main/resources/bitwisearithmetic/queen/test.";
 
     @Test
     public void RunTests() {
         String inFile;
         String outFile;
-//        for (int testNumber = 0; testNumber <= 9; testNumber++) {
-//            inFile = pathKing + testNumber + ".in";
-//            outFile = pathKing + testNumber + ".out";
-//            System.out.println("Test King #" + testNumber + ": " + RunTest(inFile, outFile, new King()));
-//        }
-//        for (int testNumber = 0; testNumber <= 9; testNumber++) {
-//            inFile = pathKnight + testNumber + ".in";
-//            outFile = pathKnight + testNumber + ".out";
-//            System.out.println("Test Knight #" + testNumber + ": " + RunTest(inFile, outFile, new Knight()));
-//        }
-//        for (int testNumber = 0; testNumber <= 9; testNumber++) {
-//            inFile = pathRook + testNumber + ".in";
-//            outFile = pathRook + testNumber + ".out";
-//            System.out.println("Test Rook #" + testNumber + ": " + RunTest(inFile, outFile, new Rook()));
-//        }
-//        for (int testNumber = 0; testNumber <= 9; testNumber++) {
-//            inFile = pathBishop + testNumber + ".in";
-//            outFile = pathBishop + testNumber + ".out";
-//            System.out.println("Test Bishop #" + testNumber + ": " + RunTest(inFile, outFile, new Bishop()));
-//        }
         for (int testNumber = 0; testNumber <= 9; testNumber++) {
-            inFile = PathQueen + testNumber + ".in";
-            outFile = PathQueen + testNumber + ".out";
+            inFile = PATH_KING + testNumber + ".in";
+            outFile = PATH_KING + testNumber + ".out";
+            System.out.println("Test King #" + testNumber + ": " + RunTest(inFile, outFile, new King()));
+        }
+        for (int testNumber = 0; testNumber <= 9; testNumber++) {
+            inFile = PATH_KNIGHT + testNumber + ".in";
+            outFile = PATH_KNIGHT + testNumber + ".out";
+            System.out.println("Test Knight #" + testNumber + ": " + RunTest(inFile, outFile, new Knight()));
+        }
+        for (int testNumber = 0; testNumber <= 9; testNumber++) {
+            inFile = PATH_ROOK + testNumber + ".in";
+            outFile = PATH_ROOK + testNumber + ".out";
+            System.out.println("Test Rook #" + testNumber + ": " + RunTest(inFile, outFile, new Rook()));
+        }
+        for (int testNumber = 0; testNumber <= 9; testNumber++) {
+            inFile = PATH_BISHOP + testNumber + ".in";
+            outFile = PATH_BISHOP + testNumber + ".out";
+            System.out.println("Test Bishop #" + testNumber + ": " + RunTest(inFile, outFile, new Bishop()));
+        }
+        for (int testNumber = 0; testNumber <= 9; testNumber++) {
+            inFile = PATH_QUEEN + testNumber + ".in";
+            outFile = PATH_QUEEN + testNumber + ".out";
             System.out.println("Test Queen #" + testNumber + ": " + RunTest(inFile, outFile, new Queen()));
         }
     }
