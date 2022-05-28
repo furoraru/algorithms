d6dfd74471093b86210f8f1c9b8fa96a3d851ed0
konstantinovaw <kamikofuroraru@gmail.com>
2022-05-28 20:11:49 +0300

    basic-structures: add MatrixArray

--- src/main/java/com/konstantinova/algorithms/Main.java
+++ src/main/java/com/konstantinova/algorithms/Main.java
@@ -1,10 +1,17 @@
 package com.konstantinova.algorithms;
 
-import com.konstantinova.algorithms.basicstructures.FactorArray;
 import com.konstantinova.algorithms.basicstructures.IArray;
+import com.konstantinova.algorithms.basicstructures.MatrixArray;
 
 public class Main {
     public static void main(String[] args) {
-        IArray<Integer> factor = new FactorArray<>();
+        IArray<Integer> matrix = new MatrixArray<>();
+        matrix.add(1);
+        matrix.add(2);
+        System.out.println(1);
+        matrix.add(3, 1);
+        System.out.println(2);
+        matrix.remove(1);
+        System.out.println(3);
     }
 }
