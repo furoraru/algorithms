189ee8a42d8917769dddfdc95500e24092712d58
konstantinovaw <kamikofuroraru@gmail.com>
2022-06-07 22:33:58 +0300

    simple-sort: add optimizedBubbleSort

--- src/main/java/com/konstantinova/algorithms/simplesort/BubbleSort.java
+++ src/main/java/com/konstantinova/algorithms/simplesort/BubbleSort.java
@@ -10,11 +10,33 @@ public class BubbleSort {
         for (int k = 0; k < N - 1; k++) {
             for (int p = 0; p < N - k - 1; p++) {
                 if (array[p] > array[p + 1]) {
+                    // swap
                     temp = array[p];
                     array[p] = array[p + 1];
                     array[p + 1] = temp;
                 }
             }
+        }
+        return array;
+    }
+
+    // Если не было обменов, значит все элементы на своих местах -> массив остортирован
+    public int[] optimizedBubbleSort(int[] array) {
+        int temp;
+        int N = array.length;
+        boolean swapped;
+        for (int k = 0; k < N - 1; k++) {
+            swapped = false;
+            for (int p = 0; p < N - k - 1; p++) {
+                if (array[p] > array[p + 1]) {
+                    // swap
+                    temp = array[p];
+                    array[p] = array[p + 1];
+                    array[p + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
         }
         return array;
     }
