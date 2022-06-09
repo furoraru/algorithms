0ee55ff8d382edde69c36322bfad70917450e65c
furoraru <kamikofuroraru@gmail.com>
2022-06-09 11:05:22 +0300

    simple-sort: add insertion shift sort, fix tests

--- src/main/java/com/konstantinova/algorithms/simplesort/InsertionSort.java
+++ src/main/java/com/konstantinova/algorithms/simplesort/InsertionSort.java
@@ -17,4 +17,31 @@ public class InsertionSort {
         }
         return array;
     }
+
+    public int[] insertionShiftSort(int[] array) {
+        int temp;
+        int N = array.length;
+        for (int k = 1; k < N; k++) {
+            temp = array[k];
+            for (int p = k; p >= 0; p--) {
+                // когда temp максимальный
+                if ((temp > array[p]) && (k - p == 1)) {
+                    break;
+                }
+                // когда temp не максимальный и мы ищем для него index
+                if ((temp > array[p])) {
+                    System.arraycopy(array, p + 1, array, p + 2, k - p - 1);
+                    array[p + 1] = temp;
+                    break;
+                }
+                // крайняя левая позиция
+                if ((temp < array[p]) && (p == 0)) {
+                    System.arraycopy(array, p, array, p + 1, k - p);
+                    array[p] = temp;
+                }
+            }
+
+        }
+        return array;
+    }
 }
--- src/test/java/com/konstantinova/algorithms/simplesort/SimpleSortTest.java
+++ src/test/java/com/konstantinova/algorithms/simplesort/SimpleSortTest.java
@@ -4,35 +4,47 @@ import org.junit.Test;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Arrays;
 import java.util.Random;
 
 public class SimpleSortTest {
     @Test
     public void RunTests() {
-        bubbleSort();
-        insertionSort();
-        shellSort();
-    }
-
-    private void bubbleSort() {
         Random random = new Random();
         int[] notSortedArray = random.ints(10000, 1, 10000).toArray();
+        int arrayLength = notSortedArray.length;
+        bubbleSort(Arrays.copyOf(notSortedArray, arrayLength));
+        optimizedBubbleSort(Arrays.copyOf(notSortedArray, arrayLength));
+        insertionSort(Arrays.copyOf(notSortedArray, arrayLength));
+        insertionShiftSort(Arrays.copyOf(notSortedArray, arrayLength));
+        shellSort(Arrays.copyOf(notSortedArray, arrayLength));
+    }
+
+    private void bubbleSort(int[] notSortedArray) {
         Instant startTime = Instant.now();
         int[] sortedArray = new BubbleSort().bubbleSort(notSortedArray);
         System.out.println("\nTime bubble sort: " + Duration.between(startTime, Instant.now()).toMillis() + "ms");
     }
 
-    private void insertionSort() {
-        Random random = new Random();
-        int[] notSortedArray = random.ints(10000, 1, 10000).toArray();
+    private void optimizedBubbleSort(int[] notSortedArray) {
+        Instant startTime = Instant.now();
+        int[] sortedArray = new BubbleSort().optimizedBubbleSort(notSortedArray);
+        System.out.println("\nTime bubble OPTIMIZED sort: " + Duration.between(startTime, Instant.now()).toMillis() + "ms");
+    }
+
+    private void insertionSort(int[] notSortedArray) {
         Instant startTime = Instant.now();
         int[] sortedArray = new InsertionSort().insertionSort(notSortedArray);
         System.out.println("\nTime insertion sort: " + Duration.between(startTime, Instant.now()).toMillis() + "ms");
     }
 
-    private void shellSort() {
-        Random random = new Random();
-        int[] notSortedArray = random.ints(10000, 1, 10000).toArray();
+    private void insertionShiftSort(int[] notSortedArray) {
+        Instant startTime = Instant.now();
+        int[] sortedArray = new InsertionSort().insertionShiftSort(notSortedArray);
+        System.out.println("\nTime insertion SHIFT sort: " + Duration.between(startTime, Instant.now()).toMillis() + "ms");
+    }
+
+    private void shellSort(int[] notSortedArray) {
         Instant startTime = Instant.now();
         int[] sortedArray = new ShellSort().shellSort(notSortedArray);
         System.out.println("\nTime shell sort: " + Duration.between(startTime, Instant.now()).toMillis() + "ms");
